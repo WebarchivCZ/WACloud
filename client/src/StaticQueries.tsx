79258816ea45d6ea91066d579a5e7c4fdb13fe12
Peter Sekan <sekan@inqool.cz>
2020-07-28 13:40:40 +0200

    Maven compilation during image build

--- client/src/StaticQueries.tsx
+++ client/src/StaticQueries.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, makeStyles, Fab, FormControl, InputLabel, Select, MenuItem, Typography, TextField, Checkbox, FormControlLabel, List, ListItem, ListItemSecondaryAction, ListItemText, IconButton, Paper } from '@material-ui/core';
+import { Grid, makeStyles, Fab, FormControl, InputLabel, Select, MenuItem, Typography, TextField, Checkbox, FormControlLabel, List, ListItem, ListItemSecondaryAction, ListItemText, IconButton, Paper, Box } from '@material-ui/core';
 import _ from 'lodash';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -72,80 +72,86 @@ function StaticQueries({queries, setQueries}:{queries:IQuery[], setQueries:(quer
         2 STATICKÉ DOTAZY
       </Typography>
       {queries.map((query, index) => (
-        <Grid container spacing={2} key={index}>
-          <Grid item md={2} xs={12}>
-            <FormControl style={{width: "100%"}}>
-              <InputLabel id="query-type">Typ dotazu</InputLabel>
-              <Select
-                labelId="query-type-label"
-                id="query-type"
-                value={query.type}
-                onChange={(event) => handleQueryTypeChange(index, event)}
-              >
-                <MenuItem value="frequency">Frequency</MenuItem>
-                <MenuItem value="colocation">Colocation</MenuItem>
-                <MenuItem value="occurence">Occurence</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item md={5} xs={12}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <FormControl style={{width: "100%"}}>
-                  <TextField 
-                    label="Zadejte text pro vyhledávaní" 
-                    value={query.searchText}
-                    onChange={(event) => handleChangeSearchText(index, event)}
-                    onKeyDown={(event) => { if(event.keyCode === 13) handleAddQueryText(index) }}
-                  />
-                </FormControl>
+        <Box my={2}>
+          <Paper>
+            <Box p={2}>
+              <Grid container spacing={2} key={index}>
+                <Grid item md={2} xs={12}>
+                  <FormControl style={{width: "100%"}}>
+                    <InputLabel id="query-type">Typ dotazu</InputLabel>
+                    <Select
+                      labelId="query-type-label"
+                      id="query-type"
+                      value={query.type}
+                      onChange={(event) => handleQueryTypeChange(index, event)}
+                    >
+                      <MenuItem value="frequency">Frequency</MenuItem>
+                      <MenuItem value="colocation">Colocation</MenuItem>
+                      <MenuItem value="occurence">Occurence</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item md={5} xs={12}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <FormControl style={{width: "100%"}}>
+                        <TextField 
+                          label="Zadejte text pro vyhledávaní" 
+                          value={query.searchText}
+                          onChange={(event) => handleChangeSearchText(index, event)}
+                          onKeyDown={(event) => { if(event.keyCode === 13) handleAddQueryText(index) }}
+                        />
+                      </FormControl>
+                    </Grid>
+                    
+                    <Grid item xs={5}>
+                      <FormControlLabel
+                        control={<Checkbox color="primary" checked={query.context} onChange={(event) => handleChangeContext(index, event)} />}
+                        label="Přidat kontext"
+                      />
+                    </Grid>
+                    
+                    <Grid item xs={7}>
+                      <FormControlLabel
+                        control={<TextField label="počet" type="number" value={query.contextLength} onChange={(event) => handleChangeContextLength(index, event)} />}
+                        label="Počet slov kontextu"
+                        labelPlacement="start"
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+                
+                <Grid item xs={12} md={1}>
+                  <Fab variant="extended" onClick={() => { handleAddQueryText(index) }}>Přidat</Fab>
+                </Grid>
+                
+                <Grid item xs={12} md={3}>
+                  <Typography variant="body1">
+                    Seznam vyrazů
+                  </Typography>
+                  <Paper>
+                    <List>
+                      {query.queries.map((q, ind) => (
+                        <ListItem key={ind} dense>
+                          <ListItemText primary={q} />
+                          <ListItemSecondaryAction>
+                            <IconButton edge="end" aria-label="comments" onClick={() => handleDeleteQueryText(index, ind)}>
+                              <DeleteIcon />
+                            </IconButton>
+                          </ListItemSecondaryAction>
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Paper>
+                </Grid>
+                
+                <Grid item xs={12} md={1} justify="flex-end" style={{textAlign: "right", paddingRight: "2rem"}}>
+                  <Fab color="primary" onClick={() => handleDeleteQuery(index)}><DeleteIcon/></Fab>
+                </Grid>
               </Grid>
-              
-              <Grid item xs={5}>
-                <FormControlLabel
-                  control={<Checkbox color="primary" checked={query.context} onChange={(event) => handleChangeContext(index, event)} />}
-                  label="Přidat kontext"
-                />
-              </Grid>
-              
-              <Grid item xs={7}>
-                <FormControlLabel
-                  control={<TextField label="počet" type="number" value={query.contextLength} onChange={(event) => handleChangeContextLength(index, event)} />}
-                  label="Počet slov kontextu"
-                  labelPlacement="start"
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-          
-          <Grid item xs={12} md={1}>
-            <Fab variant="extended" onClick={() => { handleAddQueryText(index) }}>Přidat</Fab>
-          </Grid>
-          
-          <Grid item xs={12} md={3}>
-            <Typography variant="body1">
-              Seznam vyrazů
-            </Typography>
-            <Paper>
-              <List>
-                {query.queries.map((q, ind) => (
-                  <ListItem key={ind} dense>
-                    <ListItemText primary={q} />
-                    <ListItemSecondaryAction>
-                      <IconButton edge="end" aria-label="comments" onClick={() => handleDeleteQueryText(index, ind)}>
-                        <DeleteIcon />
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                ))}
-              </List>
-            </Paper>
-          </Grid>
-          
-          <Grid item xs={12} md={1} justify="flex-end" style={{textAlign: "right", paddingRight: "2rem"}}>
-            <Fab color="primary" onClick={() => handleDeleteQuery(index)}><DeleteIcon/></Fab>
-          </Grid>
-        </Grid>
+            </Box>
+          </Paper>
+        </Box>
       ))}
       <Grid item xs={12}>
         <Fab color="primary" onClick={handleAddQuery}><AddIcon/></Fab> Přidat další dotaz
